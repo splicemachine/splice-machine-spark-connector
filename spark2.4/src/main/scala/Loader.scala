02001ef1baf19c9b8da5db87b6691f796bbe4ce9
Joe Panko <jpanko1@earthlink.net>
2020-10-30 15:42:32 -0500

    DB-9924 Latest updates.

--- spark2.4/src/main/scala/Loader.scala
+++ spark2.4/src/main/scala/Loader.scala
@@ -11,7 +11,7 @@ class Loader(
               spliceKafkaServers: String,
               spliceKafkaPartitions: String,
               dataQueue: BlockingQueue[DataFrame],
-              taskQueue: BlockingDeque[(Seq[RowForKafka], Long)],
+              taskQueue: BlockingDeque[(Seq[RowForKafka], Long, String)],
               batchRegulation: BatchRegulation,
               processing: AtomicBoolean,
               conserveTopics: Boolean = false
@@ -21,50 +21,69 @@ class Loader(
   val nsds = new SplicemachineContext(Map(
     "url" -> spliceUrl,
     "KAFKA_SERVERS" -> spliceKafkaServers,
-    "KAFKA_TOPIC_PARTITIONS" -> spliceKafkaPartitions
+    "KAFKA_TOPIC_PARTITIONS" -> spliceKafkaPartitions,
+    "USE_FLOW_MARKERS" -> "true"
   ))
   
 //  val s0 = Seq.empty[String]
 
   override def run(): Unit =
     do {
-      println(s"${java.time.Instant.now} $id LOAD check for data")
-      val df = dataQueue.take()
+      try {
+        println(s"${java.time.Instant.now} $id LOAD check for data")
+        val df = dataQueue.take()
 
-      val task =
-        if (conserveTopics && taskQueue.size > 1) {
-          val t = taskQueue.pollLast()
-          if (t == null) {
-            None
-          }
-          else if (batchRegulation.pass(t._2) && taskQueue.size > 0) {
-            Some(t)
-          } else {
-            taskQueue.put(t)
-            None
-          }
-        } else None
+        val task =
+          if (conserveTopics && taskQueue.size > 1) {
+            val t = taskQueue.pollLast()
+            if (t == null) {
+              None
+            }
+            else if ( /*batchRegulation.pass(t._2) &&*/ taskQueue.size > 0) {
+              Some(t)
+            } else {
+              taskQueue.put(t)
+              None
+            }
+          } else None
 
-      val (topicName, totalBatchCount) = if (task.isEmpty) {
-        (nsds.newTopic_streaming(), 0L)
-      } else {
-        val lastRows = task.get._1
-        if (lastRows.headOption.isDefined) {
-          nsds.sendData(lastRows)
-          (lastRows.head.topicName, task.get._2)
-        } else {
-          println(s"${java.time.Instant.now} $id LOAD WARN topic name not found")
+        var newTopic = false
+        val (topicName: String, totalBatchCount) = if (task.isEmpty) {
+          newTopic = true
           (nsds.newTopic_streaming(), 0L)
+        } else {
+          val lastRows = task.get._1
+          if (lastRows.headOption.isDefined) {
+            nsds.sendData(lastRows, false)
+            (task.get._3, task.get._2)
+          } else {
+            println(s"${java.time.Instant.now} $id LOAD WARN topic name not found")
+            newTopic = true
+            (nsds.newTopic_streaming(), 0L)
+          }
         }
+
+        println(s"${java.time.Instant.now} $id LOAD to $topicName")
+        val (lastRows: Seq[RowForKafka], batchCount: Long, ptnInfo: Array[String]) = nsds.sendData_streaming(df, topicName)
+        val newTotal = totalBatchCount + batchCount
+        
+        val topicInfo: String = if( newTopic || topicName.contains("::") ) {
+          val topicSuffix = nsds.topicSuffix(ptnInfo, spliceKafkaPartitions.toInt)
+          if( newTopic || topicSuffix.isEmpty ) { topicName+topicSuffix }
+          else {
+            val activePartitions = Set.empty[String] ++ topicSuffix.split("::")(1).split(",") ++ topicName.split("::")(1).split(",")
+            if( activePartitions.size == spliceKafkaPartitions.toInt ) { topicName }
+            else { topicName+"::"+activePartitions.mkString(",") }
+          }
+        } else { topicName }
+        
+        println(s"${java.time.Instant.now} $id LOAD batch count $newTotal")
+        taskQueue.put((lastRows, newTotal, topicInfo))
+      } catch {
+        case e: Throwable =>
+          println(s"${java.time.Instant.now} $id LOAD ERROR Load failed")
+          e.printStackTrace
       }
-
-      println(s"${java.time.Instant.now} $id LOAD to $topicName")
-      val (lastRows: Seq[RowForKafka], batchCount) = nsds.sendData_streaming(df, topicName)
-      val newTotal = totalBatchCount + batchCount
-
-      println(s"${java.time.Instant.now} $id LOAD batch count $newTotal")
-      taskQueue.put((lastRows, newTotal))
-
     } while(processing.get)
 
 }
