92f6e888b6874550ba7e9a258a04fe570de1681a
Joe Panko <jpanko1@earthlink.net>
2021-07-07 10:09:56 -0500

    DB-12042 JMS source.

---
+++ spark3.0/src/main/scala/com/splicemachine/spark/jms/ConnectionFactoryProvider.scala
@@ -0,0 +1,76 @@
+package com.splicemachine.spark.jms
+
+import java.util.Properties
+import javax.jms.ConnectionFactory
+
+import com.rabbitmq.jms.admin.RMQConnectionFactory
+import io.confluent.kafka.jms.KafkaConnectionFactory
+import org.apache.activemq.ActiveMQConnectionFactory
+
+/**
+  * Created by exa00015 on 24/12/18.
+  */
+trait ConnectionFactoryProvider {
+
+  def createConnection(options:Map[String,String]):ConnectionFactory
+
+}
+
+class AMQConnectionFactoryProvider extends ConnectionFactoryProvider {
+
+  override def createConnection(options: Map[String, String]): ConnectionFactory = {
+    new ActiveMQConnectionFactory
+  }
+}
+
+class IBMMQConnectionFactoryProvider extends ConnectionFactoryProvider {
+
+  override def createConnection(options: Map[String, String]): ConnectionFactory = {
+    import com.ibm.msg.client.wmq.common.CommonConstants
+    import com.ibm.msg.client.jms.JmsConstants
+    val cf = com.ibm.msg.client.jms.JmsFactoryFactory.getInstance(JmsConstants.WMQ_PROVIDER).createConnectionFactory
+    cf.setStringProperty(CommonConstants.WMQ_HOST_NAME, options.getOrElse("host", throw new IllegalArgumentException("Option host is required")))
+    cf.setIntProperty(CommonConstants.WMQ_PORT, options.getOrElse("port", throw new IllegalArgumentException("Option port is required")).toInt)
+    cf.setStringProperty(CommonConstants.WMQ_CHANNEL, options.getOrElse("channel", throw new IllegalArgumentException("Option channel is required")))
+    cf.setIntProperty(CommonConstants.WMQ_CONNECTION_MODE, CommonConstants.WMQ_CM_CLIENT)
+    cf.setStringProperty(CommonConstants.WMQ_QUEUE_MANAGER, options.getOrElse("queue_manager", throw new IllegalArgumentException("Option queue_manager is required")))
+    options.get("application").foreach(
+      app => cf.setStringProperty(CommonConstants.WMQ_APPLICATIONNAME, app)
+    )
+    cf.setBooleanProperty(JmsConstants.USER_AUTHENTICATION_MQCSP, true)
+    options.get("userid").foreach(
+      userid => cf.setStringProperty(JmsConstants.USERID, userid)
+    )
+    options.get("password").foreach(
+      password => cf.setStringProperty(JmsConstants.PASSWORD, password)
+    )
+    cf
+  }
+}
+
+class RMQConnectionFactoryProvider extends ConnectionFactoryProvider {
+
+  override def createConnection(options: Map[String, String]): ConnectionFactory = {
+    val connectionFactory = new RMQConnectionFactory
+    connectionFactory.setUsername(options.getOrElse("username",throw new IllegalArgumentException("Option username is required")))
+    connectionFactory.setPassword(options.getOrElse("password",throw new IllegalArgumentException("Option password is required")))
+    connectionFactory.setVirtualHost(options.getOrElse("virtualhost",throw new IllegalArgumentException("Option virtualhost is required")))
+    connectionFactory.setHost(options.getOrElse("host",throw new IllegalArgumentException("Option host is required")))
+    connectionFactory.setPort(options.getOrElse("port",throw new IllegalArgumentException("Option port is required")).toInt)
+    connectionFactory
+  }
+}
+
+class KafkaConnectionFactoryProvider extends ConnectionFactoryProvider {
+
+  override def createConnection(options: Map[String, String]): ConnectionFactory = {
+    val props = new Properties
+    props.put("bootstrap.servers", options.getOrElse("bootstrap.servers",throw new IllegalArgumentException("Option bootstrap.servers is required")))
+    props.put("zookeeper.connect", options.getOrElse("zookeeper.connect",throw new IllegalArgumentException("Option zookeeper.connect is required")))
+    props.put("client.id", options.getOrElse("client.id",throw new IllegalArgumentException("Option client.id is required" )))
+    val connectionFactory = new KafkaConnectionFactory(props)
+    connectionFactory
+  }
+
+}
+
---
+++ spark3.0/src/main/scala/com/splicemachine/spark/jms/SparkApp.scala
@@ -0,0 +1,162 @@
+package com.splicemachine.spark.jms
+
+import java.io.StringReader
+
+import javax.xml.namespace.QName
+import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLStreamConstants
+import javax.xml.stream.events.XMLEvent
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{Row, SQLContext, SaveMode}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+//import org.apache.spark.sql.types._
+
+/**
+  * Created by exa00015 on 21/12/18.
+  */
+object SparkApp {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("Sql starter")
+    val sparkContext = new SparkContext(conf)
+    val sqlContext = new SQLContext(sparkContext)
+    val sparkSession = sqlContext.sparkSession
+    import sparkSession.implicits._
+
+    val stream = sparkSession.readStream
+      .format("jk.bigdata.tech.jms")
+      .option("connection","ibmmq")
+      .option("host","stl-colo-srv006")
+      .option("port","1414")
+      .option("channel","DEV.APP.SVRCONN")
+      .option("queue_manager","QM1")
+      .option("queue","DEV.QUEUE.1")
+      .option("message_type","bytes")
+      .option("userid","app")
+      .option("password","passw0rd")
+      .option("application","SparkApp0")
+      .load
+//      .select(col("content") as "klog_xml")
+      .map( row => {
+        val xml = row.getAs[String]("content")
+        println("**********")
+        println(xml)
+        val eventReader = XMLInputFactory.newInstance.createXMLEventReader(new StringReader(xml))
+        var inKlog = false
+        var inTransaction = false
+        var inBusinessUnit = false
+        val data = collection.mutable.Map[String,String]()
+        data += "klog_xml" -> xml
+        while( data.size < 8 && eventReader.hasNext ) {
+          val event = eventReader.nextEvent
+          event.getEventType match {
+            case XMLStreamConstants.START_ELEMENT => {
+              val startElement = event.asStartElement
+              startElement.getName.getLocalPart.toUpperCase match {
+                case "KLOG" => inKlog = true
+                case "TRANSACTION" => if( inKlog ) {
+                  inTransaction = true
+                  data += ("transaction_type" -> startElement.getAttributeByName(new QName("TypeCode")).getValue)
+                }
+                case "BUSINESSUNIT" => if( inTransaction ) { inBusinessUnit = true }
+                case "UNITID" => if( inBusinessUnit ) {
+                  data += ("store_id" -> eventReader.getElementText)
+                }
+                case "RECEIPTDATETIME" => if( inTransaction ) {
+                  data += ("transaction_date" -> eventReader.getElementText)
+                }
+                case "WORKSTATIONID" => if( inTransaction ) {
+                  data += ("workstation_id" -> eventReader.getElementText)
+                }
+                case "SEQUENCENUMBER" => if( inTransaction ) {
+                  data += ("transaction_number" -> eventReader.getElementText)
+                }
+                case "POSLOGDATETIME" => if( inTransaction ) {
+                  data += ("created_ts" -> eventReader.getElementText)
+                }
+                case "OPERATORID" => if( inTransaction ) {
+                  data += ("created_by" -> eventReader.getElementText)
+                }
+                case _ => {}
+              }
+            }
+            case XMLStreamConstants.END_ELEMENT => {
+              val endElement = event.asEndElement
+              endElement.getName.getLocalPart.toUpperCase match {
+                case "KLOG" => inKlog = false
+                case "TRANSACTION" => if (inKlog) { inTransaction = false }
+                case "BUSINESSUNIT" => if (inTransaction) { inBusinessUnit = false }
+                case _ => {}
+              }
+            }
+            case _ => {}
+          }
+        }
+        Row(
+          data.getOrElse("store_id", ""),
+          data.getOrElse("transaction_date", ""),
+          data.getOrElse("workstation_id", ""),
+          data.getOrElse("transaction_number", ""),
+          data.getOrElse("transaction_type", ""),
+          data.getOrElse("klog_xml", ""),
+          data.getOrElse("created_ts", ""),
+          data.getOrElse("created_by", "")
+        )
+      }) (RowEncoder(
+          StructType(Seq(
+            StructField("store_id", StringType),
+            StructField("transaction_date", StringType),
+            StructField("workstation_id", StringType),
+            StructField("transaction_number", StringType),
+            StructField("transaction_type", StringType),
+            StructField("klog_xml", StringType),
+            StructField("created_ts", StringType),
+            StructField("created_by", StringType)
+          ))
+      ))
+
+//    BusinessUnit.UnitID — store_id string
+//    ReceiptDateTime — transaction_date timestamp
+//    WorkstationID — workstation_id numeric(8,0)
+//    SequenceNumber — transaction_number numeric(8,0)
+//    Transaction.TypeCode — transaction_type string
+//    Entire xml — klog_xml clob
+//    POSLogDateTime — created_ts timestamp
+//    OperatorID — created_by string
+
+    val query = stream.writeStream
+      .outputMode("append")
+      .format("console")
+      .start()
+
+    query.awaitTermination
+    query.stop
+    sparkSession.stop
+
+    /*
+    import sparkSession.implicits._
+    val df = sparkSession
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "host1:port1,host2:port2")
+      .option("subscribe", "connect-configs")
+      .load()
+    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+      .as[(String, String)]
+
+    val query = df.writeStream
+      .outputMode("append")
+      .format("console")
+      .start()
+    //result.show(20,false)
+    /*result.write.mode(SaveMode.Append)
+      .format("jk.bigdata.tech.jms")
+      .option("connection","jk.bigdata.tech.jms.AMQConnectionFactoryProvider")
+      .option("queue","customerQueue")
+      .save*/
+      */
+  }
+
+}
