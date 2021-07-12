480737b4ecbd955725187816100e6232d9757a93
Igor Praznik <ipraznik@splicemachine.com>
2021-07-12 17:59:24 +0200

    javadoc added

--- spark3.0/src/main/scala/com/splicemachine/spark/util/HdfsConfigurationUtil.scala
+++ spark3.0/src/main/scala/com/splicemachine/spark/util/HdfsConfigurationUtil.scala
@@ -7,10 +7,26 @@ import org.apache.hadoop.conf.Configuration
  * system via URLs, like hdfs://hdfs/tmp/
  */
 object HdfsConfigurationUtil {
+
+  /**
+   * @param conf Hadoop Configuration
+   * @param fsName dfs.nameservices value
+   * @param nameNodes ';' splitted list of namenodes, like "nn0;nn1"
+   * @param nameNodeRPCs ';' splitted list of namenode rpc addresses,
+   *                     like "splicedb-hdfs-nn-0.splicedb-hdfs-nn.test.svc.cluster.local:8020;splicedb-hdfs-nn-1.splicedb-hdfs-nn.test.svc.cluster.local:8020"
+   */
   def setHdfsConfig(conf: Configuration, fsName: String, nameNodes: String, nameNodeRPCs: String): Unit = {
     setHdfsConfig(conf: Configuration, fsName: String, nameNodes.split(";"), nameNodeRPCs.split(";"))
   }
 
+  /**
+   * @param conf Hadoop Configuration
+   * @param fsName dfs.nameservices value
+   * @param nameNodes namenodes, like "Array("nn0","nn1")
+   * @param nameNodeRPCs namenode rpc addresses,
+   *                     like Array("splicedb-hdfs-nn-0.splicedb-hdfs-nn.test.svc.cluster.local:8020",
+   *                                "splicedb-hdfs-nn-1.splicedb-hdfs-nn.test.svc.cluster.local:8020")
+   */
   def setHdfsConfig(conf: Configuration, fsName: String, nameNodes: Array[String], nameNodeRPCs: Array[String]) {
     if (nameNodes.length != nameNodeRPCs.length) {
       throw new IllegalArgumentException(s"The number of nameNodes does not correspond to nameNodeRPCs: ${nameNodes.length} / ${nameNodeRPCs.length}")
